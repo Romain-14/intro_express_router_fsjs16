4825ef0915713151dfa66895272c824f1d5b890e
Romain-14 <romain14fmsn.com>
2022-08-30 12:00:53 +0200

    initial commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 
 import router from './router/index.js';
 
+// va utiliser lors de l'instanciation de notre app le fichier sur toute les routes le fichier principal de routing
 app.use(router);
 
 app.listen(9000, ()=>{
